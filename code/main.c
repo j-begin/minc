69d7284cd630299bcb430a7fa87648b1e4bf73e3
Jesse <jbegin@protonmail.com>
2024-05-25 15:59:33 -0400

    added a basic repl

--- code/main.c
+++ code/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+#include <string.h>
 #include "lib/parser.h"
+
+/* ADD STRING LITERAL ADDITION */
 
 void show_all_atoms(struct List* list) {
 	int i = 0, j = 0;
@@ -19,8 +22,31 @@ void show_all_atoms(struct List* list) {
 		}
 	}
 }
-
 int main(int argc, char* argv[]) {
+	Bool running = True;
+
+	puts("REPL STARTED");
+
+	while (running) {
+		struct List* jit_code = NULL;
+		char code[8192];
+		size_t code_flag = 0;
+		int i;
+
+		memset(code, 0, sizeof(char) * 8192);
+		printf("* ");
+		scanf(" %8191[^\n]", code);
+		if (code[0] != '\0') {
+			if (Validate(code)) {
+				jit_code = Parse(code);
+			}
+			show_all_atoms(jit_code);
+
+			/* execute here */
+		} else {
+			running = False;
+		}
+	}
 
 	return 0;
 }
