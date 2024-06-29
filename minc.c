4f45a045e6f47eb0759fc38a8a4d8795c81d3a05
Jesse <jbegin@protonmail.com>
2024-06-29 10:22:32 -0400

    Add simple REPL
    Added a simple statically allocated REPL for later input processing of
    the language.

--- minc.c
+++ minc.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "sys_specs.h"
 
-int main(int argc, char* argv[]) {
+#define SSTR(X) #X
+#define STR(X) SSTR(X)
+#define REPL_INPUT_LENGTH 5000
+
+i32 main(i32 argc, char* argv[]) {
+	char input[REPL_INPUT_LENGTH];
+
+	puts("Starting Minc REPL");
+
+	while (1) {
+		memset(input, 0, sizeof(char) * REPL_INPUT_LENGTH);
+		printf("* ");
+		scanf(" %"STR(REPL_INPUT_LENGTH)"[^\n]", input);
+		if (input[0] == '\0') {
+			break;
+		}
+		printf("=> %s\n", input);
+	}
+
+	puts("Shutting Down Minc REPL");
+
 	return 0;
 }
